72acd21af25506885304df4ce4617a739223a644
Tauqeer-AriF <chtauqeer1117@gmail.com>
2020-03-30 10:33:57 -0700

    angular catalog search

---
+++ public-html/angular/module.js
@@ -0,0 +1,17 @@
+var a= angular.module('1st',[]);
+
+a.controller('b', function($scope){
+
+    $scope.list = [
+    {name:"T-shirt" , price:100},
+    {name:"V-shirt" , price:200},
+    {name:"O-shirt" , price:700},
+    {name:"Check-shirt" , price:400},
+    {name:"Suit-Shirt" , price:300},
+    {name:"Sports-shirt" , price:150},
+    {name:"Night-shirt" , price:100},
+    ]
+
+
+
+});
